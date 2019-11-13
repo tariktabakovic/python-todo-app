09d5820f70e6027fbeb20bd33d3b90935873619f
Tarik Tabakovic <tariktabakovic@Tariks-MacBook-Air.local>
2019-11-13 15:23:43 -0500

    adds print_todo function

--- todoapp.py
+++ todoapp.py
@@ -1,21 +1,26 @@
 # Keep track of todos using a list.
 todo_list = []
 
+# I need to print my todos
+def print_todos():
+    for todo in todo_list:
+        print(todo)
+
 # I need a way to add todos.
 def add_todo(todo):
     # We recieve a todo which is a string.
     todo_list.append(todo)
 
 # Print the empty todo list.
-print(todo_list)
+print_todos()
 # Add a todo by calling our function.
 add_todo("feed the cat")
 # Print the todo list again, making sure my todo got added.
-print(todo_list)
+print_todos()
 
 # I need to be able to delete todos.
 def delete_todo(index):
-    #del todo_list[index] another way to pop
+    #del todo_list[index] <- another way to pop
     try:
         todo_list.pop(index)
     except IndexError:
@@ -25,6 +30,6 @@ delete_todo(0)
 print(todo_list)
 delete_todo(0)
 print(todo_list)
-# I need to print my todos.
+
 
 # Show user the main menu.
