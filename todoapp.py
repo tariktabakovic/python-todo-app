c251c86771122a2457802c7092cb1871e41cd0ba
Tarik Tabakovic <tariktabakovic@Tariks-MacBook-Air.local>
2019-11-13 14:46:28 -0500

    implements add_todo function

---
+++ todoapp.py
@@ -0,0 +1,20 @@
+# Keep track of todos using a list.
+todo_list = []
+
+# I need a way to add todos.
+def add_todo(todo):
+    # We recieve a todo which is a string
+    todo_list.append(todo)
+
+# Print the empty todo list
+print(todo_list)
+# Add a todo by calling our function
+add_todo("feed the cat")
+# Print the todo list again, making sure my todo got added
+print(todo_list)
+
+# I need to be able to delete todos.
+
+# I need to print my todos.
+
+# Show user the main menu.
